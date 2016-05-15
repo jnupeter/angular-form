b3a6e9699dfc26b5cc33fc4b6ead6f0b1c24d946
Peter Cai <jnupeter@gmail.com>
2016-05-15 14:16:22 +1000

    add Title service

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,16 @@ import {HttpTestComponent} from './http/http-test.component';
 import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {HeroFormComponent} from "./hero-form.component";
 import {OtherComponent} from "./other.component";
-
+import {Title} from '@angular/platform-browser';
 
 @Component({
     selector: 'my-app',
     template: `
        <h1>Component Router (Deprecated)</h1>
        <nav>
-       <a [routerLink]="['Forms']">Forms</a>
-       <a [routerLink]="['HttpTest']">Http Test</a>
-       <a [routerLink]="['Others']" >Others</a>
+       <a [routerLink]="['Forms']" (click)="setTitle('Forms')">Forms</a>
+       <a [routerLink]="['HttpTest']" (click)="setTitle('HttpTest')">Http Test</a>
+       <a [routerLink]="['Others']" (click)="setTitle('Others')">Others</a>
 </nav>
        <div class="container">
            <router-outlet></router-outlet>
@@ -26,4 +26,12 @@ import {OtherComponent} from "./other.component";
   {path: '/httptest', name: 'HttpTest', component: HttpTestComponent},
   {path: '/other', name: 'Others', component: OtherComponent, useAsDefault: true}
 ])
-export class AppComponent { }
+export class AppComponent {
+  constructor(private titleService: Title) {
+
+  }
+
+  setTitle(title : string) {
+    this.titleService.setTitle(title);
+  }
+}
--- app/main.ts
+++ app/main.ts
@@ -2,5 +2,6 @@ import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {Title} from '@angular/platform-browser';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [Title, HTTP_PROVIDERS, ROUTER_PROVIDERS]);
