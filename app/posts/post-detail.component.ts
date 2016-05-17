20b02cecb28eefbcdbb91fab424110e2e13c924c
unknown <peter-wei.cai@QTAC-02254.qtac.edu.au>
2016-05-17 16:47:10 +1000

    add Post detail component

---
+++ app/posts/post-detail.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector : 'post-detail',
+    template: 'Details goes here'
+})
+export class PostDetailComponent {
+
+}
--- app/posts/post.component.ts
+++ app/posts/post.component.ts
@@ -1,13 +1,22 @@
 import {Component} from '@angular/core';
 import {PostListComponent} from "./post-list.component";
+import {PostDetailComponent} from "./post-detail.component";
 
 @Component({
     selector: 'post',
     template: `
         <h2>Post<h2>
-        <post-list></post-list>
+        <div class="container">
+           <div class="col-lg-6">
+               <post-list></post-list>
+           </div>
+           <div class="col-lg-6">
+               <post-detail></post-detail>
+           </div>
+        </div>
+
     `,
-    directives : [PostListComponent]
+    directives : [PostListComponent, PostDetailComponent]
 })
 export class PostComponent {
 
